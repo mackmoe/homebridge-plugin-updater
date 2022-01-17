318d738a75fd986666e59e8bf674fdf183743f47
Mo Nash <3383500+mackmoe@users.noreply.github.com>
2022-01-17 10:03:00 -0600

    create plugin updater script
    created a simple script to update homebridge plugins

---
+++ hbp-updater.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+
+#Create a txt fille list of all homebridge plugins
+pushd /usr/lib/node_modules
+PLUGIN_LIST="/home/homebridge/hb-plugins"
+
+#Create a txt fille list of all homebridge plugins
+ls -ld homebridge* | awk '{print $9}' > /home/homebridge/hb-plugins
+
+#Update the plugins on the list
+while IFS= read -r plugin;do
+  sudo -E -n npm install -g $plugin@latest;
+done < $PLUGIN_LIST
+
+#restart the hb service
+hb-service restart
+
+#Cleanup
+rm -f /home/homebridge/hb-plugins
